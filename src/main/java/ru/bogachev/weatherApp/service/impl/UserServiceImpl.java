0a4d80941d661cd8ec4b3f2e255a53f4c1c0d03f
Konstantin Bogachev <bogachev_konstantin@bk.ru>
2025-01-24 00:05:20 +0300

    Implementation of the UserService interface

---
+++ src/main/java/ru/bogachev/weatherApp/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package ru.bogachev.weatherApp.service.impl;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.bogachev.weatherApp.exception.UserNotFoundException;
+import ru.bogachev.weatherApp.model.user.User;
+import ru.bogachev.weatherApp.repository.UserRepository;
+import ru.bogachev.weatherApp.service.UserService;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public void create(@NonNull final User user) {
+        String email = user.getEmail();
+        if (userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException(
+                    String.format("Пользователь с адресом "
+                                  + "электронной почты '%s' "
+                                  + "уже существует. "
+                                  + "Проверьте введённые данные.",
+                            email)
+            );
+
+        }
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User getByEmail(@NonNull final String email) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        return optionalUser.orElseThrow(() ->
+                new UserNotFoundException(
+                        String.format("Пользователь с адресом "
+                                      + "электронной почты '%s' "
+                                      + "не был найден. "
+                                      + "Проверьте введённые данные",
+                                email)
+                )
+        );
+    }
+}
