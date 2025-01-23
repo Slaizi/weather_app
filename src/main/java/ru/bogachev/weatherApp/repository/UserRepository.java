aab6d98d091ed93640772505b5f7ce0018f8d2da
Konstantin Bogachev <bogachev_konstantin@bk.ru>
2025-01-24 01:18:32 +0300

    Implementation of UserRepository added methods: - findByEmail to search for a user by their email address. - existByEmail to check if a user with the given email already exists in the database.

--- src/main/java/ru/bogachev/weatherApp/repository/UserRepository.java
+++ src/main/java/ru/bogachev/weatherApp/repository/UserRepository.java
@@ -1,9 +1,20 @@
 package ru.bogachev.weatherApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.bogachev.weatherApp.model.user.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    @Query(value = """
+            SELECT EXISTS(SELECT 1 FROM users WHERE email = :email)
+            """, nativeQuery = true)
+    boolean existsByEmail(@Param("email") String email);
 }
