c1f85a7d8b5ff7ce13649a4e9e5ce327a567e081
Konstantin Bogachev <bogachev_konstantin@bk.ru>
2025-01-24 23:20:59 +0300

    Rename from SignInResponse to JwtResponse

--- src/main/java/ru/bogachev/weatherApp/dto/auth/JwtResponse.java
+++ src/main/java/ru/bogachev/weatherApp/dto/auth/JwtResponse.java
@@ -3,7 +3,7 @@ package ru.bogachev.weatherApp.dto.auth;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Ответ, содержащий токены доступа и обновления, используемые для аутентификации и авторизации пользователя.")
-public record SignInResponse(
+public record JwtResponse(
 
         @Schema(description = "Токен доступа, предоставляющий доступ к защищённым ресурсам системы. Имеет ограниченный срок действия.")
         String accessToken,
